d16dacf0dc00f5c9d544651fc460e07f27836735
nicholas <ncl1984@gmail.com>
2019-09-16 17:33:08 +0000

    lesson 15 completed

--- spec/controllers/grams_controller_spec.rb
+++ spec/controllers/grams_controller_spec.rb
@@ -2,6 +2,14 @@ require 'rails_helper'
 
 RSpec.describe GramsController, type: :controller do
   describe "grams#destroy action" do
+    it "shoundn't allow users who didn't create the gram to destroy it" do
+      gram = FactoryBot.create(:gram)
+      user = FactoryBot.create(:user)
+      sign_in user
+      delete :destroy, params: { id: gram.id }
+      expect(response).to have_http_status(:forbidden)
+    end
+
     it "shouldn't let unauthenticated users destroy a gram" do
       gram = FactoryBot.create(:gram)
       delete :destroy, params: { id: gram.id }
@@ -27,6 +35,14 @@ RSpec.describe GramsController, type: :controller do
 
 
   describe "grams#update action" do
+    it "shouldn't let users who didn't create the gram update it" do
+      gram = FactoryBot.create(:gram)
+      user = FactoryBot.create(:user)
+      sign_in user
+      patch :update, params: { id: gram.id, gram: { message: 'wahoo' } }
+      expect(response).to have_http_status(:forbidden)
+    end
+
     it "shouldn't let unauthenticated users update a gram" do
       gram = FactoryBot.create(:gram)
       patch :update, params: { id: gram.id, gram: { message: "Hello" } }
@@ -63,14 +79,14 @@ RSpec.describe GramsController, type: :controller do
   end
 
   describe "grams#edit action" do
-    it "shouldn't let a user who did not create the gram edit a gram" do
+    it "shouldn't let a user who did not create the gram edit a gram" do 
       gram = FactoryBot.create(:gram)
       user = FactoryBot.create(:user)
-      sign_in gram.user
-
+      sign_in user
       get :edit, params: { id: gram.id }
       expect(response).to have_http_status(:forbidden)
     end
+
 
     it "shouldn't let unauthenticated users edit a gram" do
       gram = FactoryBot.create(:gram)
